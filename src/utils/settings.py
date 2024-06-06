dfe0c84203f2b7950d951df4ff5f384273f34346
Henri Lemoine <henri@zeroth.sh>
2024-06-06 17:19:56 -0400

    Added settings.py file

---
+++ src/utils/settings.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+from pathlib import Path
+from datetime import datetime
+
+load_dotenv()
+
+# General
+PROJECT_PATH = Path(__file__).parent.parent.parent
+DATE = datetime.now().strftime('%Y-%m-%d')
+YEAR = datetime.now().strftime('%Y')
+COURSE_WEBSITE = 'https://www.mcgill.ca/study/2024-2025/courses/search' # TODO
+
+## Data
+DATA_PATH = PROJECT_PATH / 'data'
+CACHE_PATH = DATA_PATH / '.cache'
+PLOTS_PATH = DATA_PATH / 'plots'
+CSV_PATH = DATA_PATH / 'csv'
+
+## Source
+SRC_PATH = PROJECT_PATH / 'src'
+NOTEBOOKS_PATH = SRC_PATH / 'notebooks'
+
+# CSVs
+RAW_COURSES_CSV = CSV_PATH / 'McGill Courses and Prereqs9.csv'
+COURSES_CSV = CSV_PATH / 'Formatted McGill Courses and Prereqs.csv'
